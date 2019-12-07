30d6e46ef91e00058f8e3ae9e9822f98e9857f0b
github username <github email address>
2019-12-07 08:12:05 -0500

    adds quiz controller

---
+++ app/controllers/api/v1/quiz_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::QuizController < ApplicationController
+
+   # all available quizzes in DB - need to render JSON
+   def index
+      @quizzes = Quiz.all
+      render json: @quizzes, status: 200
+   end
+
+   # show a single quiz - render JSON
+   def show
+      @quiz = Quiz.find(params[:id])
+      render json: @quiz, status: 200
+   end
+
+
+end
