ea6eca6f531715ddb13fee6b526d3dac9b5866b9
github username <github email address>
2019-12-08 09:49:10 -0500

    adds additional seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,19 @@
 # test seed data 
 
 # Test Category
-categories = Category.create([{name: 'Sports', description: 'Test your sports knowledge here!'}])
+categories = Category.create([
+   {name: 'Sports', description: 'Test your sports knowledge here!'},
+   {name: 'Music', description: 'The only rule is: No Stairway to Heaven.'}
+])
 
 # Test Quiz 
-quizzes = Quiz.create([{title: 'Sports Quiz #1', description: 'A quiz about baseball!', category_id: 1}])
+quizzes = Quiz.create([
+   {title: 'Sports Quiz #1', description: 'A quiz about baseball!', category_id: 1},
+   {title: 'Music Quiz #1', description: 'Test your famous frontman knowledge!', category_id: 2}
+])
 
 # Test questions 
-questions = Question.create([{prompt: 'What Hall of Fame pitcher was a Unanimous Cy Young Award Winner in 1999, finishing with a record of 23-4 that year?', content: ['A. Roger Clemens', 'B. Randy Johnson', 'C. Pedro Martinez', 'D. Sandy Koufax'], correct_answer: 'C. Pedro Martinez', quiz_id: 1}])
+questions = Question.create([
+   {prompt: "What Hall of Fame pitcher was a Unanimous Cy Young Award Winner in 1999, finishing with a record of 23-4 that year?", content: ['A. Roger Clemens', 'B. Randy Johnson', 'C. Pedro Martinez', 'D. Sandy Koufax'], correct_answer: 'C', quiz_id: 1},
+   {prompt: "What famous lead singer was known for writing the 2013 hit 'Why'd you only call me when you're high?' ", content: ['A. Alex Turner', 'B. Liam Ghallagher', 'C. Julian Casablancas', 'D. Tupac'], correct_answer: 'A', quiz_id: 2}
+])
