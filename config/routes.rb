0513e669d196c70c08c346f432fde2e2f23915bd
github username <github email address>
2019-12-15 16:03:28 -0500

    updates routes and creates category/quizzes path

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 
-  resources :quizzes, except: [:create]
-  resources :questions
-  resources :categories
+  resources :quizzes, only:[:index, :show, :update]
+  resources :questions, only:[:index, :show, :update]
+
+  resources :categories, only:[:index, :show, :update] do
+    resources :quizzes
+  end
 
 
 end
