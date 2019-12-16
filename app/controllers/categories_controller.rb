cc2f6b92ac3e0fc3f7764f69055d8525df9154cc
github username <github email address>
2019-12-15 19:13:25 -0500

    adds strong params to categories_controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,4 +1,5 @@
 class CategoriesController < ApplicationController
+   accepts_nested_attributes_for :quizzes
 
    def index
       categories = Category.all
@@ -8,6 +9,12 @@ class CategoriesController < ApplicationController
    def show
       category = Category.find(params[:id])
       render json: CategorySerializer.new(category)
+   end
+
+   private
+
+   def category_params
+      params.require(:category).permit(:name, :description, quizzes_attributes: [:category_id, :title, :description])
    end
 
 end
